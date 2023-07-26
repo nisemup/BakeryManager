0c8801730e49c1ef04f885050a6fa7888d4ce9e6
nisemup <kornetik03@gmail.com>
2023-07-26 23:54:06 +0300

    Initial commit.

---
+++ src/main/java/com/nisemup/bakerymanager/BakeryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nisemup.bakerymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryManagerApplication.class, args);
+    }
+
+}

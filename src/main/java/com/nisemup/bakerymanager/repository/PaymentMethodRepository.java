58d4278484a5542ec2da5bae490a113caf889a91
nisemup <kornetik03@gmail.com>
2023-07-27 21:39:55 +0300

    Revert "Audit, service and repositories."
    This reverts commit 5769072fb60f59c87c38e0a98aeca97b8b936019.

--- src/main/java/com/nisemup/bakerymanager/repository/PaymentMethodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nisemup.bakerymanager.repository;
-
-import com.nisemup.bakerymanager.model.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
-}
